0f04767a45f19bf7865635d6f5ef1012726d30ab
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2022-10-16 22:40:26 +0300

    10 Soruluk Genel Kültür Test Sınavı
    .

---
+++ OnSoruluSinavSistemi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnSoruluSinavSistemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region SORULAR SINIFTAN ÇEKME
+            Sorular[] sorular = new Sorular[10];
+            sorular[0].soru = "Türkiye'nin Başkenti Neresidir ?";
+            sorular[0].islem1 = "Elazığ";
+            sorular[0].islem2 = "Eskişehir";
+            sorular[0].islem3 = "Ankara";
+            sorular[0].islem4 = "İstanbul";
+
+            sorular[1].soru = "Rusya'nın Başkenti Neresidir ?";
+            sorular[1].islem1 = "Dinamo Kiev";
+            sorular[1].islem2 = "Moskova";
+            sorular[1].islem3 = "St.Petersburg";
+            sorular[1].islem4 = "Novosibirsk";
+
+            sorular[2].soru = "Fransa'nın Başkenti Neresidir ?";
+            sorular[2].islem1 = "Bordeaux";
+            sorular[2].islem2 = "Paris";
+            sorular[2].islem3 = "Nice";
+            sorular[2].islem4 = "Marsilya";
+
+            sorular[3].soru = "Facebook'un Kurucusu Kimdir";
+            sorular[3].islem1 = "Elon Musk";
+            sorular[3].islem2 = "Steve Jobs";
+            sorular[3].islem3 = "Bill Gates";
+            sorular[3].islem4 = "Mark Zuckerberg";
+
+            sorular[4].soru = "Telefonun İcadı Kime Aittir ?";
+            sorular[4].islem1 = "Nicole Tesla";
+            sorular[4].islem2 = "Alexander Graham Bell";
+            sorular[4].islem3 = "Martin Cooper";
+            sorular[4].islem4 = "Elon Musk";
+
+            sorular[5].soru = "Selfie'nin Türkçe Anlamı Nedir ?";
+            sorular[5].islem1 = "Ön Çekim";
+            sorular[5].islem2 = "Yan Çekim";
+            sorular[5].islem3 = "Öz Çekim";
+            sorular[5].islem4 = "Fotoğraf";
+
+            
+           
+
+
+
+
+
+            #endregion
+
+
+
+        }
+    }
+}
---
+++ OnSoruluSinavSistemi/Sorular.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnSoruluSinavSistemi
+{
+    internal class Sorular
+    {
+        public string soru;
+        public string islem1;
+        public string islem2;  
+        public string islem3;
+        public string islem4;
+
+    }
+}
